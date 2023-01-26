fa6933068f08df2e270f8aec88e0a746e61a59df
Elena Naychuk <106145336+ElenaNaychuk@users.noreply.github.com>
2023-01-26 18:17:54 +0800

    redesigned a little bit

--- learn_english_words/src/components/Header/Header.jsx
+++ learn_english_words/src/components/Header/Header.jsx
@@ -1,12 +1,8 @@
-// import { useEffect, useRef } from 'react';
 import { inject, observer } from "mobx-react";
 
 import style from './header.module.scss';
 
 function Header({ user, showLoginForm }) {
-
-    // const ref = useRef(null);
-    // useEffect(() => ref.current.focus(), []);
 
     const savedUserName = localStorage.getItem('name');
     const unsavedUserName = sessionStorage.getItem('name');
@@ -21,19 +17,12 @@ function Header({ user, showLoginForm }) {
         <div className={style.container}>
             <div className={style.login_form}>
                 <div className={style.profil}>
-                    <img className={style.profil_icon}
-                        src="./assets/images/icons8-man-50.png"
-                        alt="logo" />
                     {user.name === 'пользователь'
                         ? <button onClick={showLoginForm} className={style.btn}>Войти</button>
                         : <p onClick={showLoginForm} className={style.userName}>{user.name}</p>
                     }
                 </div>
             </div>
-            {/* <legend className={style.search_block}>
-                <input ref={ref} className={style.input} type="text" placeholder='поиск' />
-                <img className={style.search_icon} src='./assets/images/searchIcon.svg' alt="search" />
-            </legend> */}
         </div>
     );
 }
